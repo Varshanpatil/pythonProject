839fee817362a7209e90d9b6cc2ec81f7c38df2e
Pritik1805 <prity1801k@gmail.com>
2024-06-14 14:51:03 +0530

    Lab01

---
+++ 03062024/Lab03.py
@@ -0,0 +1,9 @@
+#print()
+#self-concept in OOps which points to inself
+#*args - Unlimited number of arguments *-stringg,int,float,,boolean.....
+#sep ' ' - How you want to seprate the arguments
+#end-'\n'- in the end what you want to do
+#file=Name - File IO
+
+print("Hello", "world", 123,True,3.14)
+print("Hi,My name is Varsha","Patil", sep="-")
