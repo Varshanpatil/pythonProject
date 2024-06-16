757a6ded59801afe40ebf57cafd82d33a143d2f6
Pritik1805 <prity1801k@gmail.com>
2024-06-16 23:26:20 +0530

    Lab01

---
+++ 03062024/Lab04.py
@@ -0,0 +1,5 @@
+print("My Name is Varsha Patil",end="." )
+print("I am software Engineer")
+
+print("My", "name", "is", "Varsha Patil", sep="-", end="\t")
+print("I am software Engineer")
---
+++ 03062024/Lab05.py
@@ -0,0 +1,6 @@
+#Can't use sep & end args in start of the line.should use end of the line code.
+#will get error-Postion argumrnt follows keyword argument
+#print(Sep"-", "varsha")
+
+print("I am good person", end="_")
+print("I am bad person")
