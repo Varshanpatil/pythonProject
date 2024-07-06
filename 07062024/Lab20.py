89cbf997a92e02a2d6a97528369db6ad597369d1
Pritik1805 <prity1801k@gmail.com>
2024-07-06 20:12:28 +0530

    07JUne24 code practice

---
+++ 07062024/Lab20.py
@@ -0,0 +1,15 @@
+batman = "Bruce Wayne"
+#first = batman[0]  # Accessing the first character
+#print(first)
+#space = batman[5]  # Accessing the empty space in the string
+#print(space)
+#last = batman[len(batman) - 1]
+#print(last)
+
+#Revrese indexing
+batman = "Bruce Wayne"
+print(batman[-1])  # Corresponds to batman[10]
+print(batman[-5])  # Corresponds to batman[6]
+
+
+
