068e2a5a0c7bb34c7a46c9f14b69e9e938d89d20
Pritik1805 <prity1801k@gmail.com>
2024-08-05 13:21:03 +0530

    12 June 24 code practice

---
+++ 12062024/Lab37.py
@@ -0,0 +1,6 @@
+
+a = 8
+if a == 5: # = Assignment, == condition
+    print("Hello")
+else:
+    print("Bye")
---
+++ 12062024/Lab45.py
@@ -0,0 +1,11 @@
+
+age = 30
+name = "Pramod"
+
+if name == "Pramod":
+    if age == 30:
+        print("Allowed")
+    else:
+        print("Not Allowed!, wrong age")
+else:
+    print("Wrong Name")
