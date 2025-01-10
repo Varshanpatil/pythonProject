034927e93486dc68cb2916a13d666e0655902a02
Pritik1805 <prity1801k@gmail.com>
2025-01-10 17:02:49 +0530

    assignment practice

---
+++ Practice program/Py1.py
@@ -0,0 +1,9 @@
+#Take any random no in list and perform square function using list comprenhsion
+list = [1,2,3,4,5,6]
+#list_square = [i**2 for i in list]
+#print(list_square)
+
+#Take any random no and need to perform cube of th eno using list comprehnsion
+
+list_cube = [i**3 for i in list]
+print(list_cube)
---
+++ Practice program/Py2.py
@@ -0,0 +1,5 @@
+list = [11,12,13,14,15,16,17,18,19,20]
+list_square = [i**2 for i in list]
+print(list_square)
+
+#or we can write this way also
